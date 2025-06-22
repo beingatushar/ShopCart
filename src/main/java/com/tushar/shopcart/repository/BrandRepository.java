683c6869e0de38744ebda625ac28b419fca02776
Tushar Aggarwal <36700581+beingatushar@users.noreply.github.com>
2025-06-22 17:05:04 +0530

    added class for Brand
    added classes for Brand:
    - Repository

---
+++ src/main/java/com/tushar/shopcart/repository/BrandRepository.java
@@ -0,0 +1,13 @@
+package com.tushar.shopcart.repository;
+
+import com.tushar.shopcart.entity.BrandEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BrandRepository extends JpaRepository<BrandEntity, Long> {
+    Optional<BrandEntity> findByName(String name);
+}
+
